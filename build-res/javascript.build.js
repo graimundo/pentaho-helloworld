9eb2b3ce8268c50a36c28a8d6f10b50c02eb40f2
João Leonardo Carmo <joao.carmo@webdetails.pt>
2014-04-10 15:12:05 +0100

    - Removed "web" base dir, now it is only the JS app namespace specified in build.properties - Added alpha versions of plugin.xml and plugin.spring.xml

--- build-res/javascript.build.js
+++ build-res/javascript.build.js
@@ -1,7 +1,7 @@
 ({
-    appDir: "./module-scripts/pentaho-helloworld",
-    baseUrl: "web",
-    dir: "../bin/scriptOutput",
+    appDir: "./module-scripts/pentaho-helloworld-jsapp-namespace",
+    baseUrl: ".",
+    dir: "../bin/scriptOutput/pentaho-helloworld-jsapp-namespace",
     
     modules: [
         {
