28788ce501c59f801a7090b56e0ec41bc41f46b1
João Leonardo Carmo <joao.carmo@webdetails.pt>
2014-04-11 16:49:45 +0100

    - Added main.html and main.js template generation.

--- package-res/module-scripts/main-dev.js
+++
@@ -1,8 +0,0 @@
-require.config({
-	baseUrl: './js',
-	paths: {
-		jquery: '../../../js-lib/expanded/common-ui/resources/web/jquery/jquery-1.9.1.min'
-	}
-});
-
-requirejs(["app"]);
--- package-res/module-scripts/main.js
+++
@@ -1,8 +0,0 @@
-require.config({
-	baseUrl: './js',
-	paths: {
-		jquery: '../../../common-ui/resources/web/jquery/jquery-1.9.1.min'
-	}
-});
-
-requirejs(["app"]);
